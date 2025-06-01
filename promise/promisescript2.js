75d8e59ecc2d798e9c60639657c2818334dbb53e
xmasplay <yimeng.lau@gmail.com>
2025-06-01 13:11:28 +0800

    Update promisescript2.js

--- promise/promisescript2.js
+++ promise/promisescript2.js
@@ -1,1 +1,16 @@
+let myPromise1 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve("Promise 1 resolved")
+    },6000)})
 
+let myPromise2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve("Promise 2 resolved")
+    },3000)})
+
+  myPromise1.then((successMessage) => {
+    console.log("From Callback " + successMessage)
+    myPromise2.then((successMessage) => {
+      console.log("From Callback " + successMessage)
+    })
+  })
